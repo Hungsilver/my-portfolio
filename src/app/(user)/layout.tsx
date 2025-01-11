561e043c86b7f7182260f5fd2335252a6fb1912f
hungsilver <hungsilvertq@gmail.com>
2025-01-11 20:02:08 +0700

    first

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LayoutUser = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return <div>{children}</div>;
+};
+
+export default LayoutUser;
---
+++ src/app/(user)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeUserPage = () => {
+  return <div>HomeUserPage</div>;
+};
+
+export default HomeUserPage;
