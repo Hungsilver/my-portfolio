1897c23ffb9b56fe467aa22faa93f21bb305274b
hungsilver <hungsilvertq@gmail.com>
2025-01-19 21:08:08 +0700

    fiux

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,4 +1,4 @@
-import ScrollToTop from "@/components/user/helpers/scroll-to-top";
+// import ScrollToTop from "@/components/user/helpers/scroll-to-top";
 import Footer from "@/components/user/homepage-user/Footer";
 import Navbar from "@/components/user/homepage-user/Navbar";
 import React from "react";
@@ -9,7 +9,7 @@ const LayoutUser = ({ children }: Readonly<{ children: React.ReactNode }>) => {
       <main className="min-h-screen mx-auto relative px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem]">
         <Navbar />
         {children}
-        <ScrollToTop />
+        {/* <ScrollToTop /> */}
       </main>
       <Footer />
     </>
