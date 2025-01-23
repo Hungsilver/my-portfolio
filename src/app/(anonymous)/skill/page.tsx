b741b4685191bf54239b68ea0618f7ca3723216c
hungsilver <hungsilvertq@gmail.com>
2025-01-23 20:56:30 +0700

    fix icon

--- src/app/(anonymous)/skill/page.tsx
+++ src/app/(anonymous)/skill/page.tsx
@@ -7,7 +7,7 @@ const SkillPage = () => {
   return (
     <div className="py-4 lg:py-12 border-t-[1px] border-[#1a1443] flex flex-col gap-3 lg:gap-8">
       <div className="flex justify-center">
-        <div className="px-4 py-2 border border-[#1a1443] rounded-md">
+        <div className="px-4 py-2 border  border-[#1a1443] rounded-md">
           Skills
         </div>
       </div>
@@ -21,7 +21,7 @@ const SkillPage = () => {
               <div className="h-full w-full border border-[#1a1443] rounded-lg hover:scale-110 transition-all duration-300 flex flex-col items-center justify-center gap-3 py-3">
                 <div className="h-8 sm:h-10">
                   <Image
-                    src={`/svg/skills/${skill}.svg`}
+                    src={`/svg/skills/${skill.toLowerCase()}.svg`}
                     height={50}
                     width={50}
                     alt="logo"
